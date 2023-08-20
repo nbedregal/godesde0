ec9c4b19a8f14ec99f6687a21ac36fda0d8caa65
LAnicolas.bedregal <lanicolas.bedregal@LAnicolegalsMBP.homenet.cl>
2023-08-20 16:22:56 -0400

    Primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nbedregal/godesde0
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/nbedregal/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
